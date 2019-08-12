b22fe13204a573bf0c308cc1912c71ab7e545605
Iago <iagofalcom@hotmail.com>
2019-08-12 00:05:32 -0300

    Implementando a Classe Transação e Relacionando com Curso

---
+++ src/main/java/br/com/iago/microservice/microservice2/model/Transaction.java
@@ -0,0 +1,31 @@
+package br.com.iago.microservice.microservice2.model;
+
+import lombok.Data;
+import org.springframework.data.repository.cdi.Eager;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "curse_id", referencedColumnName = "id")
+    private Curso curso;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "date_of_issue")
+    private LocalDateTime dateOfIssue;
+
+
+
+
+}
